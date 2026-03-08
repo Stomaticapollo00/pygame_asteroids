037c51e189e92a6051eece0a5479c51fb7c3a209
Stomaticapollo00 <gmschaffran@hotmail.com>
2026-03-07 23:58:29 -0700

    1.0.1: Player initialization update

--- main.py
+++ main.py
@@ -1,17 +1,25 @@
 import pygame
+
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
+from player import Player
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    clock = pygame.time.Clock()
+    dt = 0
+
     while True:
         log_state()
+        screen.fill("black")
+        player.draw(screen)
+        pygame.display.flip()
         for event in pygame.event.get():
-            screen.fill("black")
-            pygame.display.flip()
             if event.type == pygame.QUIT:
                 return
+        dt = clock.tick(60) / 1000
 
     
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
